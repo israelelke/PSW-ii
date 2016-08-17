427b4f84060a81b35350e18f8f8725a3c296884e
israelelke <iferreir@cni.org.br>
2016-08-16 22:31:54 -0300

    Exercicio WEB

---
+++ PSW-ii/ProjetoFinal02/src/java/Cliente.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RA21442414
+ */
+public class Cliente {
+    
+    String nome,telefone,mail,animal,ação;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getAção() {
+        return ação;
+    }
+
+    public void setAção(String ação) {
+        this.ação = ação;
+    }
+    
+}
